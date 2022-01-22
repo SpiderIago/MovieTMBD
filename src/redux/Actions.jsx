ad069c7a2cc02839121ed796bce697da13442765
SpiderIago <98226527+SpiderIago@users.noreply.github.com>
2022-01-22 13:26:58 -0300

    Create Actions.jsx

---
+++ src/redux/Actions.jsx
@@ -0,0 +1,23 @@
+import * as actions from './ActionsTypes';
+import MoviesApi from '../services/MoviesApi';
+
+export const fetchMoviesAction = (movies) => {
+	return {
+		type: actions.FETCH_MOVIES,
+		payload: movies,
+	};
+};
+
+export const addFavouriteAction = (movie) => {
+	return {
+		type: actions.ADD_TO_FAVOURITE,
+		payload: movie,
+	};
+};
+
+export const RemoveFromFavouriteAction = (id) => {
+	return {
+		type: actions.REMOVE_FROM_FAVOURITE,
+		payload: id,
+	};
+};
